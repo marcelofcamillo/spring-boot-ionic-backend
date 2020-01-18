8289b440348d14818c419f055029e0c31bfcffce
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-01-18 13:45:16 -0300

    Projeto criado

---
+++ src/test/java/com/marcelocamillo/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelocamillo.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
